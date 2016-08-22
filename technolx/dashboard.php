0cb2eb980b7d7aa953f04cd1335cbf55b3d9c326
Ritwikjoshi <jritwik3@gmail.com>
2016-08-22 02:19:02 -0700

    Dashboard
    Day1

---
+++ technolx/dashboard.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+	<title>Dashboard</title>
+	</head>
+	<link rel="stylesheet" href="div.css" type="text/css">
+	<body>
+		<div id="header"><center><font color="white">Dashboard</font></center>
+		<button id="log">Logout</button>
+		</div>
+		<div id="content">
+			<div id="left"></div>
+			<div id="right"></div>
+			<div id="middle"></div>
+		</div>
+		<div id="footer"></div>
+	</body>
+</html>
